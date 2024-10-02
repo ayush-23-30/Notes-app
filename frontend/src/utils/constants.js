30aa9abd909d61773d8ec816783f6e0a023f17ec
ayush-23-30 <pawar85060@gmail.com>
2024-10-02 22:42:57 +0530

    url update

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:9000'
-// export const BASE_URL = 'https://notes-app-api-gilt.vercel.app/'
+// export const BASE_URL = 'http://localhost:9000'
+// export const BASE_URL = 'https://notes-app-api-gilt.vercel.app/
+export const BASE_URL = 'notes-app-server-omega.vercel.app'
