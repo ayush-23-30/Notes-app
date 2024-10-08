fe6a02cf7020984d968d2225c55dae5086dd40bc
ayush-23-30 <pawar85060@gmail.com>
2024-10-08 22:21:57 +0530

    frontend url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:9000'
+// export const BASE_URL = 'http://localhost:9000'
+export const BASE_URL = 'https://backend-app-gh4q.onrender.com/'
 
 
 // export const BASE_URL = 'https://notes-app-api-gilt.vercel.app//
