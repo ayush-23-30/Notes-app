95d739a40a7013e616dd3f94d1cfd246d7694f2c
ayush-23-30 <pawar85060@gmail.com>
2024-10-08 23:26:29 +0530

    local host

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,6 @@
-export const BASE_URL = 'http://localhost:9000'
+// export const BASE_URL = 'http://localhost:9000'
 
-// export const BASE_URL = 'https://backend-app-gh4q.onrender.com/'
+export const BASE_URL = 'https://backend-app-gh4q.onrender.com/'
+
+
+//  this is working with localhost...
