9c5697777c7e295829b819c73f106ef25c1fc277
ayush-23-30 <pawar85060@gmail.com>
2024-10-09 12:17:13 +0530

    vercel

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,7 @@
 // export const BASE_URL = 'http://localhost:9000'
 
-export const BASE_URL = 'https://backend-app-gh4q.onrender.com/'
+// export const BASE_URL = 'https://backend-app-gh4q.onrender.com/'
+export const BASE_URL = 'https://note-backend-app.vercel.app/?vercelToolbarCode=xV4BBst6d_UpbpQ'
 
 
 //  this is working with localhost...
